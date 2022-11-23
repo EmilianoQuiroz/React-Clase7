e324150fcedc2a11ed92c1d3d10a14f9fd9ad552
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-23 04:53:36 -0300

    Servidor con express

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const http = require('http');
+// const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req,res)=>{
-    res.end('Servidor corriendo')
-})
+app.get('/', (req,res) => res.send('Hello world desde Express'))
 
-server.listen(3000);
+// const server = http.createServer((req,res)=>{
+//     res.end('Servidor corriendo')
+// })
+
+app.listen(3000);
 console.log('server on port 3000');
